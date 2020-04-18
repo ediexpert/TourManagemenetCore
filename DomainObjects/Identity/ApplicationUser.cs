fb195fa746802342bdc48f6fb429f6148aafc4c3
Imran Bajwa <imranbajwa01@gmail.com>
2020-04-18 20:08:57 +0200

    applicationUser added

---
+++ DomainObjects/Identity/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthWithIdentity.DomainObjects
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<IdentityResult> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            //var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            var userIdentity = await manager.CreateAsync(this);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        // Your Extended Properties
+
+
+    }
+}
